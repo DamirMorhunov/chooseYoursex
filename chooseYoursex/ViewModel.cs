d3da8e3e6c5fa4dd1b73ab296bfdeb299d81548c
Damir Morhunov <olyawer@yahoo.com>
2023-05-08 22:06:10 +0200

    add view model base

---
+++ chooseYoursex/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chooseYoursex
+{
+    class ViewModel : VMBase
+    {
+        private double _firstSliderValue;
+        private double 
+        public double Value { get => _firstSliderValue; set { _firstSliderValue = value; OnPropertyChanged(nameof(Value)); } }
+
+    }
+}
