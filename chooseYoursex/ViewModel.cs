0a706954430ee7d3605bf93f3f72b05ec7791598
Damir Morhunov <olyawer@yahoo.com>
2023-05-09 18:01:33 +0200

    change the fonctionality

--- chooseYoursex/ViewModel.cs
+++ chooseYoursex/ViewModel.cs
@@ -11,17 +11,22 @@ namespace chooseYoursex
     {
         private RelayCommand _firstSlidervalueChanged;
         private RelayCommand _secondSlidervalueChanged;
+        private double _slider1MaxValue = 100;
+        private double _slider2MaxValue = 100;
         private double _firstSliderValue;
-        private double _secondSliderValue;
+        private double _secondSliderValue = 100;
         public double FirstSliderValue { get => _firstSliderValue; set { _firstSliderValue = value; OnPropertyChanged(nameof(FirstSliderValue)); } }
         public double SecondSliderValue { get => _secondSliderValue; set { _secondSliderValue = value; OnPropertyChanged(nameof(SecondSliderValue)); } }
+        public double Slider1MaxValue { get => _secondSliderValue; set { _secondSliderValue = value; OnPropertyChanged(nameof(Slider1MaxValue)); } }
+        public double Slider2MaxValue { get => _secondSliderValue; set { _secondSliderValue = value; OnPropertyChanged(nameof(Slider2MaxValue)); } }
+
         private void FirstSliderValueChanged(object o)
         {
-            _secondSliderValue = _firstSliderValue;
+            _secondSliderValue = _slider1MaxValue - _secondSliderValue;
         }
         private void SecondSliderValueChanged(object o)
         {
-            _firstSliderValue = _secondSliderValue;
+            _firstSliderValue = _slider2MaxValue - _firstSliderValue;
         }
         public ViewModel()
         {
